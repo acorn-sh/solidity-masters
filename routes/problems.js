450855232fa3a6e2f2ec2739ba5626ac002cc075
five <niip42@gmail.com>
2024-11-10 04:48:52 +0900

    database update logic

--- routes/problems.js
+++ routes/problems.js
@@ -1,12 +1,12 @@
-// // routes/problems.js
-// const express = require('express');
-// const db = require('../config/database');
-// const router = express.Router();
+// routes/problems.js
+const express = require('express');
+const db = require('../config/database');
+const router = express.Router();
 
-// // Fetch all problems
-// router.get('/', async (req, res) => {
-//   const result = await db.query('SELECT * FROM problems');
-//   res.json(result.rows);
-// });
+// Fetch all problems
+router.get('/', async (req, res) => {
+  const result = await db.query('SELECT * FROM problems');
+  res.json(result.rows);
+});
 
-// module.exports = router;
+module.exports = router;
